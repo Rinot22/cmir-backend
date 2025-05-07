8cc72f653ec2c0fdb488cefcfdaf922d6e49c1f7
Darya <darya.martyniuk@student.pk.edu.pl>
2025-05-07 22:10:43 +0200

    init commit

---
+++ src/main/java/com/martyniuk/crimbackend/CrimBackendApplication.java
@@ -0,0 +1,13 @@
+package com.martyniuk.crimbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrimBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrimBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/martyniuk/crimbackend/CrimBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.martyniuk.crimbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrimBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
